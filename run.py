1b176c02e94f55b585d2276fb6409f70412fcdb5
root <root@hhu.novalocal>
2018-04-13 17:43:44 +0800

    merge

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/<page_name>')
+def static_file(page_name):
+    return render_template('{page_name}.html'.format(page_name=page_name))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True, debug=True)
