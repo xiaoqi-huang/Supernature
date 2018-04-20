f8b9adf0eb3e61d9a2485a10181f469a125be666
Xiaoqi Huang <hwangxiaoqi@gmail.com>
2018-04-21 01:18:45 +1000

    extract header and footer

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
+# from flask.ext.mysql import MySQL
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +9,13 @@ def home():
 @app.route('/<page_name>')
 def static_file(page_name):
     return render_template('{page_name}.html'.format(page_name=page_name))
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    response = request.post()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True, debug=True)
