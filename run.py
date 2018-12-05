63bdb7dde02e609886ddfb0dbeedf790a796df75
Xiaoqi Huang <xhua7314@uni.sydney.edu.au>
2018-12-06 00:01:21 +0800

    Add grayscale effect on the homepage; Restruct the members page, add effects

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from models import db, User
-from form import SignupForm, LoginForm
+# from models import db, User
+# from form import SignupForm, LoginForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/superusers'
-db.init_app(app)
-
-app.secret_key = "development-key"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/superusers'
+# db.init_app(app)
+#
+# app.secret_key = "development-key"
 
 @app.route('/')
 def index():
@@ -17,51 +17,51 @@ def index():
 def static_file(page_name):
     return render_template('{page_name}.html'.format(page_name=page_name))
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if 'email' in session:
-      return redirect(url_for('home'))
-
-    form = SignupForm()
-
-    if request.method == 'POST':
-        if form.validate() == False:
-            return render_template('signup.html', form=form)
-        else:
-            newuser=User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
-            db.session.add(newuser)
-            db.session.commit()
-
-            session['email'] = newuser.email
-            return redirect(url_for('home'))
-
-    elif request.method == 'GET':
-        return render_template('signup.html', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if 'email' in session:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if request.method == 'POST':
-        if form.validate() == False:
-            return render_template('login.html', form=form)
-        else:
-            email = form.email.data
-            password = form.password.data
-
-            user = User.query.filter_by(email=email).first()
-            if user is not None and user.check_password(password):
-                session['emial'] = form.email.data
-                return redirect(url_for('home'))
-            else:
-                return redirect(url_for('login'))
-    elif request.method == 'GET':
-      return render_template('login.html', form=form)
-@app.route('/logout')
-def logout():
-    session.pop('email', None)
-    return redirect(url_for('index'))
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     if 'email' in session:
+#       return redirect(url_for('home'))
+#
+#     form = SignupForm()
+#
+#     if request.method == 'POST':
+#         if form.validate() == False:
+#             return render_template('signup.html', form=form)
+#         else:
+#             newuser=User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+#             db.session.add(newuser)
+#             db.session.commit()
+#
+#             session['email'] = newuser.email
+#             return redirect(url_for('home'))
+#
+#     elif request.method == 'GET':
+#         return render_template('signup.html', form=form)
+#
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if 'email' in session:
+#         return redirect(url_for('home'))
+#     form = LoginForm()
+#     if request.method == 'POST':
+#         if form.validate() == False:
+#             return render_template('login.html', form=form)
+#         else:
+#             email = form.email.data
+#             password = form.password.data
+#
+#             user = User.query.filter_by(email=email).first()
+#             if user is not None and user.check_password(password):
+#                 session['emial'] = form.email.data
+#                 return redirect(url_for('home'))
+#             else:
+#                 return redirect(url_for('login'))
+#     elif request.method == 'GET':
+#       return render_template('login.html', form=form)
+# @app.route('/logout')
+# def logout():
+#     session.pop('email', None)
+#     return redirect(url_for('index'))
 
 #do not have home now
 @app.route("/home", methods=["GET", "POST"])
