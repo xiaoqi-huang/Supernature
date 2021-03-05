79867b02fa3b8f40ad8e8a6ad18c5f0cd32115dd
Xiaoqi Huang <xhua7314@uni.sydney.edu.au>
2021-03-06 00:33:42 +1100

    Add api get_blog

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, redirect, request, url_for
+import markdown
+
 from db import get_db
+
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-@bp.route('/blog/list/<sort>/<int:page>', methods=('GET', ))
+
+@bp.route('/blog/list/<sort>/<int:page>', methods=('GET',))
 def get_blog_list(sort='updatedAt', page=0):
 
     query = '''SELECT article.id AS aid, title, createdAt, updatedAt, user.id AS uid, user.name AS author
@@ -22,6 +26,37 @@ def get_blog_list(sort='updatedAt', page=0):
                      'createAt': row['createdAt'].isoformat(),
                      'updateAt': row['updatedAt'].isoformat(),
                      'uid': row['uid'],
-                     'author': row['author'] })
+                     'author': row['author']})
 
     return jsonify(data)
+
+
+@bp.route('/blog/<int:aid>', methods=('GET'))
+def get_blog(aid):
+
+    query = '''SELECT id AS aid, title, content, createAt, updateAt, user.id AS uid, user.name AS author
+               FROM article, user
+               WHERE article.author=user.id AND article.id=%d''' % (aid,)
+
+    db = get_db()
+    res = db.execute(query).fetchone()
+
+    if not res:
+        return redirect(url_for('index.u404'))
+    else:
+        data = {
+            'aid': res['aid'],
+            'title': res['title'],
+            'content': markdown.markdown(res['content']),
+            'createAt': row['createdAt'].isoformat(),
+            'updateAt': row['updatedAt'].isoformat(),
+            'uid': row['uid'],
+            'author': row['author']
+        }
+
+        return jsonify(data)
+
+@bp.route('/blog/add', method=('POST'))
+def add_blog():
+    # TODO
+    return None
